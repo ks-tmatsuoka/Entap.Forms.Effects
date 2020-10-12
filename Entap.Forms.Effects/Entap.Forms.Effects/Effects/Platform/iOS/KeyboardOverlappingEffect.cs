240ca9eee6adf77147992bab197bd0525e34901b
Iga <iga@entap.co.jp>
2020-10-12 10:42:11 +0900

    [add]KeyboardOverlappingEffect

---
+++ Entap.Forms.Effects/Entap.Forms.Effects/Effects/Platform/iOS/KeyboardOverlappingEffect.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Entap.Forms.Effects.Platform.iOS
+{
+    /// <summary>
+    /// キーボード表示時に、指定されたViewのMarginを調整してキーボードとの重なりを避ける
+    /// </summary>
+    public static class KeyboardOverlappingEffect
+    {
+        #region BottomMargin
+        /// <summary>
+        /// キーボード表示時のViewとKeyboardとのMargin
+        /// </summary>
+        public static readonly BindableProperty BottomMarginProperty =
+            BindableProperty.CreateAttached("BottomMargin", typeof(double), typeof(KeyboardOverlappingEffect), 2d, propertyChanged: OnPropertyChanged);
+
+        public static double GetBottomMargin(BindableObject view)
+        {
+            return (double)view.GetValue(BottomMarginProperty);
+        }
+        #endregion
+
+        #region IsEnabled
+        /// <summary>
+        /// Effectを有効にするか
+        /// </summary>
+        public static readonly BindableProperty IsEnabledProperty =
+            BindableProperty.CreateAttached("IsEnabled", typeof(bool), typeof(KeyboardOverlappingEffect), false, propertyChanged: OnPropertyChanged);
+
+        public static bool GetIsEnabled(BindableObject view)
+        {
+            return (bool)view.GetValue(IsEnabledProperty);
+        }
+        #endregion
+
+        static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is View view))
+                return;
+
+            var toRemove = view.Effects.FirstOrDefault((arg) => arg is KeyboardOverlappingRoutingEffect);
+            if (toRemove != null)
+                view.Effects.Remove(toRemove);
+            if (GetIsEnabled(bindable))
+                view.Effects.Add(new KeyboardOverlappingRoutingEffect());
+        }
+
+        class KeyboardOverlappingRoutingEffect : RoutingEffect
+        {
+            public KeyboardOverlappingRoutingEffect() : base("Entap.Forms.Effects." + nameof(KeyboardOverlappingEffect))
+            {
+            }
+        }
+    }
+}
---
+++ Entap.Forms.Effects/Entap.Froms.Effects.iOS/Effects/KeyboardOverlappingPlatformEffect.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Entap.Forms.Effects.Platform.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(Entap.Froms.Effects.iOS.KeyboardOverlappingPlatformEffect), nameof(KeyboardOverlappingEffect))]
+namespace Entap.Froms.Effects.iOS
+{
+    public class KeyboardOverlappingPlatformEffect : PlatformEffect
+    {
+        // KeyboardObserver
+        NSObject _keyboardShownObserver;
+        NSObject _keyboardHiddenObserver;
+
+        Thickness _margin;
+        double _absoluteY = -1;
+        double _parentHeight;
+
+        protected override void OnAttached()
+        {
+            SubscribeKeyboardObserver();
+        }
+
+        protected override void OnDetached()
+        {
+            UnsubscribeKeyboardObserver();
+        }
+
+        void SubscribeKeyboardObserver()
+        {
+            _keyboardShownObserver = UIKeyboard.Notifications.ObserveWillShow(OnKeyboardShown);
+            _keyboardHiddenObserver = UIKeyboard.Notifications.ObserveWillHide(OnKeyboardHidden);
+        }
+
+        void UnsubscribeKeyboardObserver()
+        {
+            _keyboardShownObserver?.Dispose();
+            _keyboardHiddenObserver?.Dispose();
+        }
+
+        void OnKeyboardShown(object sender, UIKeyboardEventArgs e)
+        {
+            if (Element is null) return;
+            if (!(Element is View view)) return;
+
+            if (_absoluteY < 0)
+            {
+                // ViewがMaringを退避
+                _margin = view.Margin;
+
+                // Viewの絶対位置Yと、Pageの高さを取得
+                _absoluteY = view.Y;
+                _parentHeight = view.Height;
+                var element = view.Parent;
+                while (element is VisualElement visualElement)
+                {
+                    _absoluteY += visualElement.Y;
+                    _parentHeight = visualElement.Height;
+                    element = visualElement.Parent;
+
+                    // NavigationPageはスキップ
+                    // NavigationPageを処理すると以下の不整合が生じる
+                    // ・_absoluteYにはNavigaitonBar.Heightが含まれない
+                    // ・_parentHeightにはNavigationBar.Heightが含まれる
+                    if (element is Page) break;
+                }
+            }
+            var bottomMargin = KeyboardOverlappingEffect.GetBottomMargin(Element);
+            var bottom = e.FrameEnd.Height -
+                (_parentHeight - _absoluteY - view.Height - view.TranslationY) +
+                bottomMargin;
+            if (bottom <= 0) return;
+
+            view.Margin = new Thickness(
+                _margin.Left,
+                _margin.Top,
+                _margin.Right,
+                bottom);
+        }
+
+        void OnKeyboardHidden(object sender, UIKeyboardEventArgs e)
+        {
+            if (Element is null) return;
+            if (!(Element is View view)) return;
+
+            // Viewが保有していたMaringに戻す
+            view.Margin = _margin;
+        }
+    }
+}
+
