3e5bf800603315402fb1b5595f07b440b7ed066a
Iga <iga@entap.co.jp>
2020-10-12 10:43:07 +0900

    [add]iOS / Android : Platform 初期化用メソッド定義

---
+++ Entap.Forms.Effects/Entap.Froms.Effects.Android/Platform.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms.Internals;
+
+namespace Entap.Froms.Effects.Android
+{
+    [Preserve(AllMembers = true)]
+    public static class Platform
+    {
+        public static void Init() { }
+    }
+}
---
+++ Entap.Forms.Effects/Entap.Froms.Effects.iOS/Platform.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xamarin.Forms.Internals;
+
+namespace Entap.Froms.Effects.iOS
+{
+    [Preserve(AllMembers = true)]
+    public static class Platform
+    {
+        public static void Init() { }
+    }
+}
