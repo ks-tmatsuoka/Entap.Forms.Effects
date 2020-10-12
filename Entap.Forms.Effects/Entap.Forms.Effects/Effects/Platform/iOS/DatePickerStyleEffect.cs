dcd96d8cd26aa361af0e2e6ec0a5d392a29e2ded
Iga <iga@entap.co.jp>
2020-10-12 11:53:32 +0900

    [add]iOS : DatePickerStyleEffect追加

---
+++ Entap.Forms.Effects/Entap.Forms.Effects/Effects/Platform/iOS/DatePickerStyleEffect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Entap.Forms.Effects.Platform.iOS
+{
+    /// <summary>
+    /// iOS14で追加されたDatePickerStyleを指定する
+    /// </summary>
+    public static class DatePickerStyleEffect
+    {
+        #region DatePickerStyle
+        public static readonly BindableProperty DatePickerStyleProperty =
+            BindableProperty.CreateAttached("DatePickerStyle", typeof(DatePickerStyle), typeof(DatePickerStyleEffect), DatePickerStyle.Automatic, propertyChanged: OnPropertyChanged);
+
+        public static DatePickerStyle GetDatePickerStyle(BindableObject view)
+        {
+            return (DatePickerStyle)view.GetValue(DatePickerStyleProperty);
+        }
+        #endregion
+
+        static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (!(bindable is View view))
+                return;
+
+            var toRemove = view.Effects.FirstOrDefault((arg) => arg is DatePickerStyleRoutingEffect);
+            if (toRemove != null)
+                view.Effects.Remove(toRemove);
+
+            view.Effects.Add(new DatePickerStyleRoutingEffect());
+        }
+
+        class DatePickerStyleRoutingEffect : RoutingEffect
+        {
+            public DatePickerStyleRoutingEffect() : base("Entap.Forms.Effects." + nameof(DatePickerStyleEffect))
+            {
+            }
+        }
+    }
+
+    /// <summary>
+    /// スタイル
+    /// https://developer.apple.com/documentation/uikit/uidatepickerstyle?language=objc
+    /// </summary>
+    public enum DatePickerStyle
+    {
+        Automatic,
+        Compact,
+        Inline,
+        Wheels
+    }
+}
---
+++ Entap.Forms.Effects/Entap.Froms.Effects.iOS/Effects/DatePickerStylePlatformEffect.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Entap.Forms.Effects.Platform.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportEffect(typeof(Entap.Froms.Effects.iOS.DatePickerStylePlatformEffect), nameof(DatePickerStyleEffect))]
+namespace Entap.Froms.Effects.iOS
+{
+    /// <summary>
+    /// DatePickerStyleEffect　Platform実装
+    /// </summary>
+    public class DatePickerStylePlatformEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            UpdateDatePickerStyle();
+        }
+
+
+        protected override void OnDetached()
+        {
+        }
+
+        void UpdateDatePickerStyle()
+        {
+            if (!UIDevice.CurrentDevice.CheckSystemVersion(14, 0)) return;
+            if (Control is null) return;
+
+            if (!(Control is UITextField textField)) return;
+            if (!(textField.InputView is UIDatePicker datePicker)) return;
+            datePicker.PreferredDatePickerStyle = GetDatePickerStyle();
+        }
+
+        UIDatePickerStyle GetDatePickerStyle()
+        {
+            var style = DatePickerStyleEffect.GetDatePickerStyle(Element);
+            switch(style)
+            {
+                case DatePickerStyle.Compact:
+                    return UIDatePickerStyle.Compact;
+                case DatePickerStyle.Inline:
+                    return UIDatePickerStyle.Inline;
+                case DatePickerStyle.Wheels:
+                    return UIDatePickerStyle.Wheels;
+                case DatePickerStyle.Automatic:
+                default:
+                    return UIDatePickerStyle.Automatic;
+            }
+        }
+    }
+}
