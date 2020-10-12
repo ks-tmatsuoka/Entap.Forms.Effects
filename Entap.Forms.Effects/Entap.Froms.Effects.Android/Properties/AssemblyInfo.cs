640d28d391ea668870e72d73c57b8d0d10d73664
Iga <iga@entap.co.jp>
2020-10-12 09:21:14 +0900

    [update]AssemblyInfo : ResolutionGroupName登録

--- Entap.Forms.Effects/Entap.Froms.Effects.Android/Properties/AssemblyInfo.cs
+++ Entap.Forms.Effects/Entap.Froms.Effects.Android/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
+using Xamarin.Forms;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -24,3 +25,5 @@ using Android.App;
 //      Revision
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ResolutionGroupName("Entap.Forms.Effects")]
